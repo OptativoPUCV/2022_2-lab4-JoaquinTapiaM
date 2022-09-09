3244622537b5ebec1f744d59a254ff33dd940f2d
Replit user <>
2022-09-09 12:37:21 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -54,11 +54,13 @@ void enlarge(HashMap * map) {
 
 
 HashMap * createMap(long capacity) {
-  long i;
+    long i;
     HashMap* new = (HashMap*)calloc(1,sizeof(HashMap));
+  
     for (i=0;i<capacity;i++){
-      new->buckets[i]->key= NULL;
+      new->*buckets[i]= NULL;
     }
+  
     new->current = -1;
     return new;
 }
