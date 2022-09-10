5a67247181ae29a8706a2c812aabbaaf54c52687
Replit user <>
2022-09-10 17:08:54 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -169,9 +169,11 @@ Pair * nextMap(HashMap * map) {
   long i = map->current;
   while(1){
     i++;
-    if(map->buckets[i]->key!=NULL){
-      map->current = i;
-      return map->buckets[i];
+    if(map->buckets[i]!=NULL){
+        if(map->buckets[i]->key!=NULL){
+          map->current = i;
+          return map->buckets[i];
+        }
     }
   }
 }
